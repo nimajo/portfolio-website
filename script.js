82cd74e6519d1c9d8089f352aff4349c47323219
nimajo <nimajokar@hotmail.co.uk>
2024-01-14 09:34:28 +0000

    Styling Upgrades, fixed mobile, removed clutter, home screen effects

--- script.js
+++ script.js
@@ -27,3 +27,37 @@ function toggleMenu() {
     var popup = document.getElementById('popup');
     popup.style.display = 'none';
   }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Get the element by ID
+    var titleElement = document.getElementById("title");
+  
+    // Initial title text
+    var titles = [
+      "BSc Computer Science Graduate",
+      "Digital Forensic Analyst Intern",
+    ];
+  
+    // Function to update the title text with typing effect
+    function updateTitle(index) {
+      titleElement.textContent = titles[index];
+      titleElement.classList.add("typing-effect");
+      setTimeout(function () {
+        titleElement.classList.remove("typing-effect");
+      }, 3000); // Adjust the duration based on your preference
+    }
+  
+    // Function to loop through the titles
+    function loopTitles() {
+      var index = 0;
+      updateTitle(index);
+      setInterval(function () {
+        index = (index + 1) % titles.length;
+        updateTitle(index);
+      }, 6000); // Adjust the interval based on your preference
+    }
+  
+    // Start the loop
+    loopTitles();
+  });
+  
